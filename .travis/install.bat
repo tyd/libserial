3d51be2ef6f2348858ec87f904fec7169ef204ca
Jeff Ctor <jeffctor@gmail.com>
2018-11-04 11:46:36 +0800

    try to fix windows ci #1

--- .travis/install.bat
+++
@@ -1,6 +0,0 @@
-set CNC_INSTALL_CNCA0_CNCB0_PORTS=YES
-set CNC_INSTALL_COMX_COMX_PORTS=YES
-set CNC_INSTALL_SKIP_SETUP_PREINSTALL=NO
-com0com-setup.exe /S
-cd "C:\Program Files (x86)\com0com"
-setupc install Portname=COM10 Portname=COM11
--- .travis/install.sh
+++ .travis/install.sh
@@ -4,15 +4,18 @@ set -x
 
 if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
   # create virtual com using com0com in windows
-
+  export TEST_INPUT_PTY=COM10
+  export TEST_OUTPUT_PTY=COM11
   # download com0com with git-bash 
   curl -fsSL https://sourceforge.net/projects/com0com/files/latest/download -o com0com.zip
   7z e com0com.zip *x64*.exe
   mv *x64*.exe com0com-setup.exe
+  export CNC_INSTALL_CNCA0_CNCB0_PORTS=YES
+  export CNC_INSTALL_COMX_COMX_PORTS=YES
+  export CNC_INSTALL_SKIP_SETUP_PREINSTALL=NO
   # use cmd.exe to run install.bat
-  powershell -c start -verb runas cmd '/C .travis\install.bat /D "%V"'
-  export TEST_INPUT_PTY=COM10
-  export TEST_OUTPUT_PTY=COM11
+  cmd '/C com0com-setup.exe /S'
+  cmd '/C C:\Program Files (x86)\com0com\setupc install Portname=COM10 Portname=COM11'
 else
   # create virtual pty
   make pty_start
