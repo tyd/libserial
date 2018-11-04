34b176edb0c46ad58ce099c8ceac58741b8e2a0a
Jeff Ctor <jeffctor@gmail.com>
2018-11-04 11:22:24 +0800

    debug install.sh, try to fix windows build failure

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
   # create virtual com using com0com in windows
 
@@ -8,7 +10,7 @@ if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
   7z e com0com.zip *x64*.exe
   mv *x64*.exe com0com-setup.exe
   # use cmd.exe to run install.bat
-  powershell -c start -verb runas cmd '/C start /D "%V" install.bat'
+  powershell -c start -verb runas cmd '/C .travis\install.bat /D "%V"'
   export TEST_INPUT_PTY=COM10
   export TEST_OUTPUT_PTY=COM11
 else
@@ -18,3 +20,5 @@ else
   export TEST_INPUT_PTY=$(cat socat.out | cut -d\  -f7 | sed -n 1p)
   export TEST_OUTPUT_PTY=$(cat socat.out | cut -d\  -f7 | sed -n 2p)
 fi
+
+set +x
