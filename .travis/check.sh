3478f542906c74a264e733ee19dfb6528c0f4570
Jeff Ctor <jeffctor@gmail.com>
2018-11-07 10:56:01 +0800

    CI: fix check.sh syntax

--- .travis/check.sh
+++ .travis/check.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [[ "$TRAVIS_OS_NAME" != "windows" ]]; then 
-  if [[ "$(stty speed < ${TEST_OUTPUT_PTY})" != "${TEST_BAUD_RATE}" ]] then
+  if [[ "$(stty speed < ${TEST_OUTPUT_PTY})" != "${TEST_BAUD_RATE}" ]]; then
     exit 1
   fi
 fi
